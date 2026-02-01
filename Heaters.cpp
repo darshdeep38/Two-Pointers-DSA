1a0f96a23e5c6ff7c4d820e4c637e4958a6ef31d
Darshdeep Singh <115945757+darshdeep38@users.noreply.github.com>
2026-02-01 18:00:02 +0530

    added another solution at the bottom

--- Heaters.cpp
+++ Heaters.cpp
@@ -16,3 +16,46 @@ public:
         return radius;
     }
 };
+
+// another way 
+
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+        vector<int> res(
+            houses.size(),
+            INT_MAX); // For each house, what is the minimum distance to any
+                      // heater? So ans array is of size of Houses array
+                      // EVERY ELEMENT IS INITIALLLY INT_MAX because we'll
+                      // update it with smaller values then we'll find the
+                      // maximum of those values
+
+        // RHS min distance calculation
+        for (int i = 0, h = 0; i < houses.size() && h < heaters.size();) {
+            if (houses[i] <= heaters[h]) {
+                res[i] = heaters[h] - houses[i];
+                i++;
+            } else
+                h++;
+        }
+
+        // LHS min distance calculation
+        for (int i = houses.size() - 1, h = heaters.size() - 1;
+             i >= 0 && h >= 0;) {
+            if (houses[i] >= heaters[h]) {
+                res[i] = min(houses[i] - heaters[h], res[i]);
+                i--;
+            } else
+                h--;
+        }
+        int maxRadius = INT_MIN;
+        for (int i = 0; i < res.size(); i++) {
+            if (maxRadius < res[i])
+                maxRadius = res[i];
+        }
+        return maxRadius;
+    }
+};
+explain me this code 
